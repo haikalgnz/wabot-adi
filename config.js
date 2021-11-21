c2c01fd0c12f21965c9c782cdcab169c91ee06f8
haikalgnz <haikalofc8@gmail.com>
2021-11-21 11:44:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,28 +1,45 @@
 const os = process
 global.DeveloperMode = 'false' //true Or false
-global.owner = ['33623746892', '6289504585790'] || os.env["OWNER_NUMBER"]// Put your number here
-global.linkGC = ['http://bit.ly/Grupgabutbotv1']
-global.mods = ['33623746892', '6289504585790'] // Want some help?
-global.prems = ['33623746892', '6289504585790'] // Premium user has unlimited limit
+global.owner = ['6289696454872', '6289696454872'] || os.env["OWNER_NUMBER"]// Put your number here
+global.linkGC = ['-']
+global.mods = ['6289696454872', '6289696454872'] // Want some help?
+global.prems = ['6289696454872', '6289696454872'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
   nrtm: 'https://nurutomo.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz',
   xteam: 'https://api.xteam.xyz',
-  vhtear: 'https://vhtear.com',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zekais: 'http://zekais-api.herokuapp.com',
   zeks: 'https://api.zeks.xyz',
-  clph: 'https://recoders-area.caliph.repl.co'
+  vhtear: 'https://api.vhtear.com',
+  lolhum: 'https://api.lolhuman.xyz',
+  fxc7: 'https://fxc7-api.herokuapp.com',
+  aqul: 'https://api.justaqul.xyz'
+
 }
-global.APIKeys = { // APIKey Here
+global.APIKeys = { // APIKey nya disini
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'ISI APIKEY LU',
-  'https://recoders-area.caliph.repl.co': 'FreeApi',
-  'https://vhtear.com': 'AdiOfficial404',
-  'https://api.zeks.xyz': 'apivinz'
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://pencarikode.xyz': 'APIKEY',
+  'https://api.xteam.xyz': 'HIRO',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'FVe0JFCdME58YSKmhoDWdRtyuJt',
+  'https://api.lolhuman.xyz': '929b48b9aa5a91abed8526cd',
+  'https://api.vhtear.com': 'sayahafiz',
+  'https://fxc7-api.herokuapp.com': 'uN8rsK4g',
+  'https://api.justaqul.xyz': '5kbUqdG00OXelFYx',
+  'http://zekais-api.herokuapp.com': 'grqgD6pU'
 }
 
+
 // Sticker WM
-global.packname = 'Sticker Dari Bot Adi'
-global.author = 'GH: Adi-OfficialL'
+global.packname = 'Sticker Dari Bot Ikal'
+global.author = 'GH: Ikal-botz'
 
 global.multiplier = 69 // The higher, The harder levelup
 
